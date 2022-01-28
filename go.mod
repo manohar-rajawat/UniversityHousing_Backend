60ded2ca752e49a37df3175395262e95312e74ef
Manohar Singh Rajawat <manoharsingh1920@gmail.com>
2022-01-28 00:47:54 -0800

    refactor(main): changing port address.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/manohar-rajawat/UniversityHousing_Backend
+module github.com/manohar-rajawat/universityhousing-backend
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi Jai Mahakal, I love %s!", r.URL.Path[1:])
+	io.WriteString(w, "Hello, world!\n")
 }
 
+// Route declaration
+func router() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+	return r
+}
+
+// Initiate web server
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := router()
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         ":9100",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
