420b5bf5e09f841c7f77a7fe59d82ec153e76174
Manohar Singh Rajawat <manoharsingh1920@gmail.com>
2022-01-28 16:03:49 -0800

    Init(Project): Init project file.

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<h1>Welcome to my web server!</h1>"))
 	})
