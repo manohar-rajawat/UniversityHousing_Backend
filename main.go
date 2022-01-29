670f0b917197c1f17ecc20f493dcc9cc8471771b
Manohar Singh Rajawat <manoharsingh1920@gmail.com>
2022-01-28 16:08:10 -0800

    Init(Project): Init project file.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	mux := http.NewServeMux()
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -14,5 +15,5 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<h1>Welcome to my web server!</h1>"))
 	})
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
