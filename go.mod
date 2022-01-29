198918e47c36bec590e6f8eb696adcb242b16c61
Manohar Singh Rajawat <manoharsingh1920@gmail.com>
2022-01-28 16:29:20 -0800

    Init(Project): Init project file.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/manohar-rajawat/universityhousing
 
 go 1.17
 
-require github.com/lib/pq v1.10.4 // indirect
+require (
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/lib/pq v1.10.4 // indirect
+)
--- main.go
+++ main.go
@@ -4,15 +4,29 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
+func getEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading environment variable file")
+	}
+	return os.Getenv(key)
+}
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("<h1>Jai Mahakal! Server</h1>"))
 }
 
 func main() {
 	mux := http.NewServeMux()
-	port := os.Getenv("PORT")
+	port := getEnvVariable("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
