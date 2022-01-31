d9f1a9ca68b5a13dfde95b7deef4f7d672727d14
Manohar Singh Rajawat <manoharsingh1920@gmail.com>
2022-01-30 16:42:38 -0800

    feat(Main): Main file for University API Handling.

--- main.go
+++ main.go
@@ -1,43 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
+	"github.com/manohar-rajawat/universityhousing/router"
 )
 
-func getEnvVariable(key string) string {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading environment variable file")
-	}
-	return os.Getenv(key)
-}
-
-func redirect(url string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, url, 301)
-	}
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("<h1>Jai Mahakal! Server</h1>"))
-}
-
 func main() {
-	mux := http.NewServeMux()
-	staticHandler := http.FileServer(http.Dir("./assets"))
 	port := os.Getenv("PORT")
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		log.Fatal("Port Not Found")
 	}
-	mux.Handle("/assets/", http.StripPrefix("/assets/", staticHandler))
-	mux.HandleFunc("/", indexHandler)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	r := router.Router()
+	fmt.Println("Starting server on the port " + port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
