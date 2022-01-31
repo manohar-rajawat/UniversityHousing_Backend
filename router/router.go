0181e4b4645e1a671c4360209849e671c09ca10d
Manohar Singh Rajawat <manoharsingh1920@gmail.com>
2022-01-30 16:39:16 -0800

    feat(Router): Creating router for handling HTTP requests.

---
+++ router/router.go
@@ -0,0 +1,14 @@
+package router
+
+import (
+	"github.com/manohar-rajawat/universityhousing/middleware"
+
+	"github.com/gorilla/mux"
+)
+
+// Router is exported and used in main.go
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/api/university/{name}", middleware.GetUniversity).Methods("GET", "OPTIONS")
+	return router
+}
